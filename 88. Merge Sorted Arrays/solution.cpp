06b1813ef706f658b0cedcadd0c35b7422e48652
nourbatniji <nour.albattniji@gmail.com>
2026-03-22 01:32:00 +0200

    Merging two sorted arrays

---
+++ 88. Merge Sorted Arrays/solution.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = n + m - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+};
+
+int main() {
+    Solution sol;
+
+    vector<int> nums1 = {4,5,6,0,0,0};
+    vector<int> nums2 = {1,2,3};
+
+    int m = 3, n = 3;
+
+    sol.merge(nums1, m, nums2, n);
+
+    for (int x : nums1)
+        cout << x << " ";
+
+    return 0;
+}
